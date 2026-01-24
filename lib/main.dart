16b7022c8383b34c7fe2e7f25752ce72f6f571e1
jefferson-Felipy <jeffersonthm1@gmail.com>
2026-01-23 21:48:33 -0300

    adicionando novos componentes para a aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:open_editor/components/transaction_chart.dart';
+import 'package:open_editor/components/transaction_form.dart';
 import 'package:open_editor/components/transaction_list.dart';
 import 'package:open_editor/components/transaction_total.dart';
 import 'package:open_editor/models/transaction.dart';
@@ -19,7 +20,7 @@ class HomePageApp extends StatelessWidget {
         colorScheme: ColorScheme(
           brightness: Brightness.dark,
           primary: Colors.black87,
-          onPrimary: const Color.fromARGB(31, 36, 36, 36),
+          onPrimary: const Color.fromARGB(31, 156, 156, 156),
           secondary: Colors.white,
           onSecondary: const Color.fromARGB(26, 177, 176, 176),
           error: Colors.red,
@@ -59,60 +60,64 @@ class OpenEditorAppState extends State<OpenEditorApp> {
   //Lista de transações_
   final List<Transaction> _transactions = [
     Transaction(
-      id: '',
+      id: '1',
       value: 56.2,
-      categoria: '',
-      carteira: '',
-      date: DateTime.now(),
-      descricao: '',
-      nota: '',
-    ),
-    Transaction(
-      id: '',
-      value: 23.2,
-      categoria: '',
-      carteira: '',
+      categoria: 'Besteiras',
+      carteira: 'Carteira',
       date: DateTime.now().subtract(Duration(days: 2)),
-      descricao: '',
-      nota: '',
+      descricao: 'Sorvete a noite',
+      nota: 'Comprei sorvete ',
     ),
     Transaction(
-      id: '',
-      value: 10.2,
-      categoria: '',
-      carteira: '',
-      date: DateTime.now().subtract(Duration(days: 3)),
-      descricao: '',
-      nota: '',
-    ),
-    Transaction(
-      id: '',
-      value: 43.2,
-      categoria: '',
-      carteira: '',
+      id: '2',
+      value: 125.23,
+      categoria: 'Carro',
+      carteira: 'Cartao',
       date: DateTime.now().subtract(Duration(days: 1)),
-      descricao: '',
-      nota: '',
+      descricao: 'Carro de formula 1',
+      nota: 'Comprei um carro',
     ),
     Transaction(
-      id: '',
-      value: 95.2,
-      categoria: '',
-      carteira: '',
-      date: DateTime.now().subtract(Duration(days: 4)),
-      descricao: '',
-      nota: '',
+      id: '3',
+      value: 22.3,
+      categoria: 'Salgadinho',
+      carteira: 'Carteira',
+      date: DateTime.now(),
+      descricao: 'Comprei salgadinhp',
+      nota: 'Salgadinho',
     ),
   ];
 
   //Lista de transações da mesma semana filtrada usando o where()_
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((tr) {
+      return tr.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
+  void sheetModalBottom() {
+    showModalBottomSheet(
+      elevation: 6,
+      isDismissible: false,
+      enableDrag: false,
+      isScrollControlled: true,
+      context: context,
+      builder: (_) {
+        return TransactionForm();
+      },
+    );
+  }
+
+  void _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) {
+        return tr.id == id;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final List<Transaction> _recentTransactions = _transactions.where((tr) {
-      return tr.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
-    }).toList();
-
     final MediaQueryData mediaQuery = MediaQuery.of(context);
 
     final AppBar appBar = AppBar(
@@ -144,6 +149,7 @@ class OpenEditorAppState extends State<OpenEditorApp> {
     final bodyPage = SafeArea(
       child: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             //Chart_
             SizedBox(
@@ -156,7 +162,13 @@ class OpenEditorAppState extends State<OpenEditorApp> {
               child: TransactionTotal(),
             ),
             //Lista de Transações
-            SizedBox(height: heightWidgetPage * 0.6, child: TransactionList()),
+            SizedBox(
+              height: heightWidgetPage * 0.6,
+              child: TransactionList(
+                transactions: _recentTransactions,
+                removeTransaction: _removeTransaction,
+              ),
+            ),
           ],
         ),
       ),
@@ -166,7 +178,7 @@ class OpenEditorAppState extends State<OpenEditorApp> {
       appBar: appBar,
       body: bodyPage,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => sheetModalBottom(),
         child: Icon(Icons.add, color: const Color.fromARGB(255, 0, 0, 0)),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
