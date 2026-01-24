97e339f3ae6d9eae4c9d5b2e6d4400c112f8ac72
jefferson-Felipy <jeffersonthm1@gmail.com>
2026-01-23 21:50:21 -0300

    Definindo a lista de transações

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:open_editor/components/transaction_item.dart';
+import 'package:open_editor/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  const TransactionList({super.key});
+  final List<Transaction> transactions;
+  final void Function(String) removeTransaction;
+
+  const TransactionList({
+    super.key,
+    required this.transactions,
+    required this.removeTransaction,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +18,26 @@ class TransactionList extends StatelessWidget {
       color: Theme.of(context).colorScheme.primary,
       child: Container(
         padding: EdgeInsets.all(10),
-        margin: EdgeInsets.all(10),
-        width: double.infinity,
         decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [],
+          children: [
+            Center(child: Text('Transações')),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (context, index) {
+                  final tr = transactions[index];
+
+                  return TransactionItem(
+                    key: GlobalObjectKey(tr),
+                    tr: tr,
+                    removeTransaction: removeTransaction,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
